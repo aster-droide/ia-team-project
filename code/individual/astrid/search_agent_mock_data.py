272fe3a05d2625cb20a16bb8569d6dbfcf0f2161
aster-droide <astridvantoor@gmail.com>
2023-05-26 12:41:20 +0100

    add mock data

---
+++ code/individual/astrid/search_agent_mock_data.py
@@ -0,0 +1,113 @@
+search_results = {
+  "search_metadata": {
+    "id": "647087c47bea27bdeab1b1e6",
+    "status": "Success",
+    "json_endpoint": "https://serpapi.com/searches/dc4bf100eefffa87/647087c47bea27bdeab1b1e6.json",
+    "created_at": "2023-05-26 10:19:48 UTC",
+    "processed_at": "2023-05-26 10:19:48 UTC",
+    "google_scholar_url": "https://scholar.google.com/scholar?q=bananas&hl=en&num=2",
+    "raw_html_file": "https://serpapi.com/searches/dc4bf100eefffa87/647087c47bea27bdeab1b1e6.html",
+    "total_time_taken": 1.3
+  },
+  "search_parameters": {
+    "engine": "google_scholar",
+    "q": "bananas",
+    "hl": "en",
+    "num": "2"
+  },
+  "search_information": {
+    "organic_results_state": "Results for exact spelling",
+    "total_results": 1140000,
+    "time_taken_displayed": 0.03,
+    "query_displayed": "bananas"
+  },
+  "organic_results": [
+    {
+      "position": 0,
+      "title": "Bananas and plantains",
+      "result_id": "dyZ4bDycdLQJ",
+      "type": "Book",
+      "link": "https://books.google.com/books?hl=en&lr=&id=0GxOPqj2UVYC&oi=fnd&pg=PR5&dq=bananas&ots=fRAeFORzsb&sig=1YRs1lHY30DgfzqDWglTlzu77Ec",
+      "snippet": "\u2026 created by the European CMO in 1993 (see \u2018Factors Influencing World Trade\u2019 in this chapter), EU banana prices became the highest of all banana markets. Despite this high price, the \u2026",
+      "publication_info": {
+        "summary": "JC Robinson, VG Sa\u00faco - 2010 - books.google.com"
+      },
+      "inline_links": {
+        "serpapi_cite_link": "https://serpapi.com/search.json?engine=google_scholar_cite&q=dyZ4bDycdLQJ",
+        "cited_by": {
+          "total": 1179,
+          "link": "https://scholar.google.com/scholar?cites=13003189807457248887&as_sdt=5,34&sciodt=0,34&hl=en&num=2",
+          "cites_id": "13003189807457248887",
+          "serpapi_scholar_link": "https://serpapi.com/search.json?as_sdt=5%2C34&cites=13003189807457248887&engine=google_scholar&hl=en&num=2"
+        },
+        "related_pages_link": "https://scholar.google.com/scholar?q=related:dyZ4bDycdLQJ:scholar.google.com/&scioq=bananas&hl=en&num=2&as_sdt=0,34",
+        "serpapi_related_pages_link": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=related%3AdyZ4bDycdLQJ%3Ascholar.google.com%2F",
+        "versions": {
+          "total": 9,
+          "link": "https://scholar.google.com/scholar?cluster=13003189807457248887&hl=en&num=2&as_sdt=0,34",
+          "cluster_id": "13003189807457248887",
+          "serpapi_scholar_link": "https://serpapi.com/search.json?as_sdt=0%2C34&cluster=13003189807457248887&engine=google_scholar&hl=en&num=2"
+        }
+      }
+    },
+    {
+      "position": 1,
+      "title": "The evolution of the bananas.",
+      "result_id": "l2aiAreIuHsJ",
+      "link": "https://www.cabdirect.org/cabdirect/abstract/19630303919",
+      "snippet": "Botanists and geneticists will welcome this book, which has been written primarily for them, and, in effect, supplements the author's more general account of the bananas as a crop \u2026",
+      "publication_info": {
+        "summary": "NW Simmonds - The Evolution of the Bananas., 1962 - cabdirect.org"
+      },
+      "inline_links": {
+        "serpapi_cite_link": "https://serpapi.com/search.json?engine=google_scholar_cite&q=l2aiAreIuHsJ",
+        "cited_by": {
+          "total": 956,
+          "link": "https://scholar.google.com/scholar?cites=8915025781984487063&as_sdt=5,34&sciodt=0,34&hl=en&num=2",
+          "cites_id": "8915025781984487063",
+          "serpapi_scholar_link": "https://serpapi.com/search.json?as_sdt=5%2C34&cites=8915025781984487063&engine=google_scholar&hl=en&num=2"
+        },
+        "related_pages_link": "https://scholar.google.com/scholar?q=related:l2aiAreIuHsJ:scholar.google.com/&scioq=bananas&hl=en&num=2&as_sdt=0,34",
+        "serpapi_related_pages_link": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=related%3Al2aiAreIuHsJ%3Ascholar.google.com%2F",
+        "versions": {
+          "total": 6,
+          "link": "https://scholar.google.com/scholar?cluster=8915025781984487063&hl=en&num=2&as_sdt=0,34",
+          "cluster_id": "8915025781984487063",
+          "serpapi_scholar_link": "https://serpapi.com/search.json?as_sdt=0%2C34&cluster=8915025781984487063&engine=google_scholar&hl=en&num=2"
+        },
+        "cached_page_link": "https://scholar.googleusercontent.com/scholar?q=cache:l2aiAreIuHsJ:scholar.google.com/+bananas&hl=en&num=2&as_sdt=0,34"
+      }
+    }
+  ],
+  "pagination": {
+    "current": 1,
+    "next": "https://scholar.google.com/scholar?start=2&q=bananas&hl=en&num=2&as_sdt=0,34",
+    "other_pages": {
+      "2": "https://scholar.google.com/scholar?start=2&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "3": "https://scholar.google.com/scholar?start=4&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "4": "https://scholar.google.com/scholar?start=6&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "5": "https://scholar.google.com/scholar?start=8&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "6": "https://scholar.google.com/scholar?start=10&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "7": "https://scholar.google.com/scholar?start=12&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "8": "https://scholar.google.com/scholar?start=14&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "9": "https://scholar.google.com/scholar?start=16&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "10": "https://scholar.google.com/scholar?start=18&q=bananas&hl=en&num=2&as_sdt=0,34"
+    }
+  },
+  "serpapi_pagination": {
+    "current": 1,
+    "next_link": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=2",
+    "next": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=2",
+    "other_pages": {
+      "2": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=2",
+      "3": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=4",
+      "4": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=6",
+      "5": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=8",
+      "6": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=10",
+      "7": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=12",
+      "8": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=14",
+      "9": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=16",
+      "10": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=18"
+    }
+  }
+}
