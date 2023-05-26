8920ab9c07a04e05b488d93b6c3fef8f29a9288d
aster-droide <astridvantoor@gmail.com>
2023-05-26 18:08:33 +0100

    agents code v2

--- code/individual/astrid/search_agent_mock_data.py
+++ code/individual/astrid/search_agent_mock_data.py
@@ -1,4 +1,4 @@
-search_results = {
+mock_1 = {
   "search_metadata": {
     "id": "647087c47bea27bdeab1b1e6",
     "status": "Success",
@@ -76,6 +76,92 @@ search_results = {
           "serpapi_scholar_link": "https://serpapi.com/search.json?as_sdt=0%2C34&cluster=8915025781984487063&engine=google_scholar&hl=en&num=2"
         },
         "cached_page_link": "https://scholar.googleusercontent.com/scholar?q=cache:l2aiAreIuHsJ:scholar.google.com/+bananas&hl=en&num=2&as_sdt=0,34"
+      }
+    }
+  ],
+  "pagination": {
+    "current": 1,
+    "next": "https://scholar.google.com/scholar?start=2&q=bananas&hl=en&num=2&as_sdt=0,34",
+    "other_pages": {
+      "2": "https://scholar.google.com/scholar?start=2&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "3": "https://scholar.google.com/scholar?start=4&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "4": "https://scholar.google.com/scholar?start=6&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "5": "https://scholar.google.com/scholar?start=8&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "6": "https://scholar.google.com/scholar?start=10&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "7": "https://scholar.google.com/scholar?start=12&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "8": "https://scholar.google.com/scholar?start=14&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "9": "https://scholar.google.com/scholar?start=16&q=bananas&hl=en&num=2&as_sdt=0,34",
+      "10": "https://scholar.google.com/scholar?start=18&q=bananas&hl=en&num=2&as_sdt=0,34"
+    }
+  },
+  "serpapi_pagination": {
+    "current": 1,
+    "next_link": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=2",
+    "next": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=2",
+    "other_pages": {
+      "2": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=2",
+      "3": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=4",
+      "4": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=6",
+      "5": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=8",
+      "6": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=10",
+      "7": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=12",
+      "8": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=14",
+      "9": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=16",
+      "10": "https://serpapi.com/search.json?as_sdt=0%2C34&engine=google_scholar&hl=en&num=2&q=bananas&start=18"
+    }
+  }
+}
+
+mock_2 = {
+  "search_metadata": {
+    "id": "647087c47bea27bdeab1b1e6",
+    "status": "Success",
+    "json_endpoint": "https://serpapi.com/searches/dc4bf100eefffa87/647087c47bea27bdeab1b1e6.json",
+    "created_at": "2023-05-26 10:19:48 UTC",
+    "processed_at": "2023-05-26 10:19:48 UTC",
+    "google_scholar_url": "https://scholar.google.com/scholar?q=bananas&hl=en&num=2",
+    "raw_html_file": "https://serpapi.com/searches/dc4bf100eefffa87/647087c47bea27bdeab1b1e6.html",
+    "total_time_taken": 1.3
+  },
+  "search_parameters": {
+    "engine": "google_scholar",
+    "q": "bananas",
+    "hl": "en",
+    "num": "2"
+  },
+  "search_information": {
+    "organic_results_state": "Results for exact spelling",
+    "total_results": 1140000,
+    "time_taken_displayed": 0.03,
+    "query_displayed": "bananas"
+  },
+  "organic_results": [
+    {
+      "position": 0,
+      "title": "Population biology of plants.",
+      "result_id": "JC4Acibs_4kJ",
+      "link": "https://www.cabdirect.org/cabdirect/abstract/19782321379",
+      "snippet": "The first chapter is concerned with experiments, analogies and models. There are sections on dispersal, dormancy and recruitment of seedling populations, effects of neighbours, effects of predators and natural dynamics of plant populations, and plants, vegetation and evolution. There …",
+      "publication_info": {
+        "summary": "JL Harper - Population biology of plants., 1977 - cabdirect.org"
+      },
+      "inline_links": {
+        "serpapi_cite_link": "https://serpapi.com/search.json?engine=google_scholar_cite&q=JC4Acibs_4kJ&token=a57f24e24842c7de",
+        "cited_by": {
+          "total": 14003,
+          "link": "https://scholar.google.com/scholar?cites=9943926152122871332&as_sdt=5,38&sciodt=0,38&hl=en",
+          "cites_id": "9943926152122871332",
+          "serpapi_scholar_link": "https://serpapi.com/search.json?cites=9943926152122871332&engine=google_scholar&hl=en"
+        },
+        "related_pages_link": "https://scholar.google.com/scholar?q=related:JC4Acibs_4kJ:scholar.google.com/&scioq=biology&hl=en&as_sdt=0,38",
+        "serpapi_related_pages_link": "https://serpapi.com/search.json?as_sdt=0%2C38&engine=google_scholar&hl=en&q=related%3AJC4Acibs_4kJ%3Ascholar.google.com%2F",
+        "versions": {
+          "total": 6,
+          "link": "https://scholar.google.com/scholar?cluster=9943926152122871332&hl=en&as_sdt=0,38",
+          "cluster_id": "9943926152122871332",
+          "serpapi_scholar_link": "https://serpapi.com/search.json?cluster=9943926152122871332&engine=google_scholar&hl=en"
+        },
+        "cached_page_link": "https://scholar.google.comhttps://scholar.googleusercontent.com/scholar?q=cache:JC4Acibs_4kJ:scholar.google.com/+biology&hl=en&as_sdt=0,38"
       }
     }
   ],
